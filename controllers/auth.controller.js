c0a2f0d622ebfe774fbc8a81e0f830266667e00d
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2023-10-23 16:14:42 +0200

    skeleton

---
+++ controllers/auth.controller.js
@@ -0,0 +1,15 @@
+
+
+function getSignup (req, res) {
+    res.render('signup')
+};
+
+function getLogin (req, res) {
+    res.render('login');
+};
+
+
+module.exports = {
+    getSignup: getSignup,
+    getLogin: getLogin,
+};
