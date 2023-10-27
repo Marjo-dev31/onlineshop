9342ff8d6b5c6a6364cad586a53539b3ec575ec5
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2023-10-27 16:34:06 +0200

    validation

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const User = require('../models/user.model');
 const authUtil = require('../util/authentification');
+const validation = require('../util/validation');
 
 function getSignup(req, res) {
     res.render('customer/auth/signup')
@@ -9,6 +10,19 @@ async function signup(req, res, next) {
     // if (!res.locals.isAuth) {
     //     return res.status('401').render('401');
     // };
+
+    if (!validation.userIsValid(
+        req.body.email,
+        req.body.password,
+        req.body.fullname,
+        req.body.street,
+        req.body.postal,
+        req.body.city)
+        || validation.emailIsConfirmed(req.body.email, req.body['confirm-email'])
+    ) {
+        res.redirect('/signup');
+        return;
+    };
 
     const user = new User(
         req.body.email,
@@ -20,6 +34,12 @@ async function signup(req, res, next) {
     );
 
     try {
+        const existsAlready = await user.existsAlready();
+
+    if(existsAlready) {
+        res.redirect('/signup');
+        return;
+    };
         await user.signup();
     } catch (error) {
         next(error);
@@ -38,7 +58,7 @@ async function login(req, res, next) {
     const user = new User(req.body.email, req.body.password);
 
     let existingUser;
-    
+
     try {
         existingUser = await user.getUserWithSameEmail();
     } catch (error) {
---
+++ util/validation.js
@@ -0,0 +1,29 @@
+function isEmpty(value) {
+    return !value || value.trim() === '';
+};
+
+
+function userCredentialsAreValid(email, password) {
+ return (
+    email && email.includes('@') && password && password.trim().length > 5
+ );
+};
+
+function userIsValid(email, password, fullname, street, postal, city) {
+    return (
+        userCredentialsAreValid(email, password) &&  
+        !isEmpty(fullname) &&
+        !isEmpty(street) &&
+        !isEmpty(postal) &&
+        !isEmpty(city)
+    );
+};
+
+function emailIsConfirmed(email, confirmEmail) {
+    return email === confirmEmail
+}
+
+module.export = {
+    userIsValid: userIsValid,
+    emailIsConfirmed: emailIsConfirmed
+};
