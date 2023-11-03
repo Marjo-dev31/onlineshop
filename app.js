8e6b72b14f89b73a6c0d74f4ba13358ff3f87108
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2023-11-03 18:34:56 +0100

    product model and item

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static('public'));
+app.use('/products/assests',express.static('product-data'));
 app.use(express.urlencoded({ extended: false })); /* gere les data qui sont en piece jointes des requetes*/
 
 const sessionConfig = session.createSessionConfig();
--- controllers/admin.controllers.js
+++ controllers/admin.controllers.js
@@ -1,14 +1,36 @@
-function getProducts(req, res) {
-    res.render('admin/products/all-products');
+const Product = require('../models/product.model');
+
+async function getProducts(req, res, next) {
+    try {
+        const products = await Product.findAll();
+        res.render('admin/products/all-products', {products: products});
+
+    } catch(error) {
+        next(error);
+        return;
+    };
 };
 
 function getNewProduct(req, res) {
     res.render('admin/products/new-product');
 };
 
-function createNewProduct(req, res) {
+async function createNewProduct(req, res, next) {
     console.log(req.body);
     console.log(req.file);
+
+    const product = new Product({
+        ...req.body,
+        image: req.file.filename,
+    });
+
+    try {
+        await product.save();
+    } catch (error) {
+        next(error);
+        return;
+    };
+
     res.redirect('/admin/products');
 };
 
