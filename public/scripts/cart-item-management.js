39da4c7dfe563b5688189bd7dfe09756924efb4c
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2023-11-16 17:41:21 +0100

    delete article of cart

--- public/scripts/cart-item-management.js
+++ public/scripts/cart-item-management.js
@@ -35,9 +35,14 @@ async function updateCartItem(event) {
 
     const responseData = await response.json();
 
+    if (responseData.updatedCartData.updatedItemPrice === 0) {
+        const articleElement = document.querySelector('.cart-item');
+        articleElement.remove();
+    } else {
+
     const cartItemTotalPriceElement = form.parentElement.querySelector('.cart-item-price');
     cartItemTotalPriceElement.textContent = responseData.updatedCartData.updatedItemPrice.toFixed(2);
-
+}
     const cartTotalPriceElement = document.getElementById('cart-total-price');
     cartTotalPriceElement.textContent = responseData.updatedCartData.newTotalPrice.toFixed(2);
 
